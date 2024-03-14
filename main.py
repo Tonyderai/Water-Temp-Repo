8be46f0828466c0b93f6fc372ed85af1e2b899a5
Gertrude Bennett <gertrudebennett15@gmail.com>
2024-03-14 10:15:30 +0000

    Added defensive programming to handle potential errors or invalid inputs

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+try:
 # Ask the user to input the water temperature
 temperature = float(input("Enter the temperature of water: "))
 
-# Create print statements if water temperature is below 0
-if temperature <= 0:
-    print("The state of the water is: Ice")
-elif temperature <= 100:
-    print("The state of the water is: Liquid Water")
+# Ensure the temperature is within a reasonable range
+if temperature < -273.15:
+    print("Error: Temperature cannot be below absolute zero.")
 else:
-    print("The state of the water is: Water vapour")
+    # Create print statements based on the water temperature
+    if temperature <= 0:
+        print("The state of the water is: Ice")
+    elif temperature <= 100:
+        print("The state of the water is: Liquid Water")
+    else:
+        print("The state of the water is: Water vapour")
