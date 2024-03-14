e15c4dc2ad6c6c189fec384490e1ee1427d6d39d
Gertrude Bennett <gertrudebennett15@gmail.com>
2024-03-14 08:42:56 +0000

    Added comments to explain what is happening in the code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# Ask user input water temperature
 temperature = float(input("Enter the temperature of water: "))
 
+# Based on user input print out the state of the water
 if temperature <= 0:
     print("The state of the water is: Ice")
 elif temperature <= 100:
